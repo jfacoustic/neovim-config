183ac9bacdf092d6ffdddd1d4cff6210e601c4b8
Joshua Mathews <joshfeltonm@gmail.com>
2025-09-03 12:10:52 -0600

    Add trouble and rename symbol remap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ return {
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = "Replace Symbol" })
 		keymap.set("n", "<C-p>", "<cmd>Telescope git_files<cr>", { desc = "Find git files" })
 	end,
 }
